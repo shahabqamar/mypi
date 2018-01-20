b24b094f5183276a34be978e6a2e4047f90582f9
¨shahabqamar¨ <shahab.qamar@gmail.com>
2018-01-20 16:15:03 +1100

    webserver boilerplate

---
+++ led-websocket/index.js
@@ -0,0 +1,16 @@
+var http = require("http").createServer(handler);
+var fs = require("fs");
+
+http.listen(8080);
+
+function handler(req, res) {
+  fs.readFile(__dirname + "/public/index.html", function(err, data) {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      return res.end("404 not found!");
+    }
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(data);
+    return res.end();
+  });
+}
